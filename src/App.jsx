780452bfb9aeb7859d944e35e384808be2079f8b
predevho <dlagusgh7894@gmail.com>
2026-03-18 14:32:01 +0900

    등록 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { useState } from 'react'
+
 function App() {
-    return <>helloworld!</>
+    const [todos, setTodos] = useState(['할일1', '할일2', '할일3'])
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault()
+        const todo = e.target.todo.value
+        setTodos([todo, ...todos])
+    }
+    return (
+        <>
+            <form onSubmit={handleOnSubmit}>
+                <input type="text" name="todo" />
+                <button>추가</button>
+            </form>
+            <ul>
+                {todos.map((todo, index) => (
+                    <li key={index}>{todo}</li>
+                ))}
+            </ul>
+        </>
+    )
 }
 
 export default App
