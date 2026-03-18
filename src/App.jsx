dd0ddd8d21aa08bdeea8524730ea51c3827fbbfb
predevho <dlagusgh7894@gmail.com>
2026-03-18 15:26:52 +0900

    toggle todo 함수 및 할일 체크 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from 'react'
 
 function App() {
-    const [todos, setTodos] = useState(['할일1', '할일2', '할일3'])
+    const [todos, setTodos] = useState([
+        { todo: '할일1', completed: true },
+        { todo: '할일2', completed: false },
+        { todo: '할일3', completed: false },
+    ])
 
     const handleOnSubmit = (e) => {
         e.preventDefault()
@@ -11,10 +15,17 @@ function App() {
             return
         }
 
-        setTodos([form.todo.value, ...todos])
+        setTodos([{ todo: form.todo.value, completed: false }, ...todos])
     }
     const deleteTodo = (selectedIndex) => {
         const nextState = todos.filter((todo, index) => index !== selectedIndex)
+        setTodos(nextState)
+    }
+
+    const toggleTodo = (selectedIndex) => {
+        const nextState = todos.map((todo, index) =>
+            index === selectedIndex ? { ...todo, completed: !todo.completed } : todo,
+        )
         setTodos(nextState)
     }
 
@@ -27,7 +38,9 @@ function App() {
             <ul>
                 {todos.map((todo, index) => (
                     <li key={index}>
-                        <span>{todo}</span>
+                        {JSON.stringify(todo.completed)}
+                        <input type="checkbox" onChange={() => toggleTodo(index)} checked={todo.completed} />
+                        <span>{todo.todo}</span>
                         <button onClick={() => deleteTodo(index)}>X</button>
                     </li>
                 ))}
