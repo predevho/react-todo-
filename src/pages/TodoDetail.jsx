142258c827f56f011c3b46fcd6b560ea9a2451b5
predevho <dlagusgh7894@gmail.com>
2026-03-19 15:33:17 +0900

    finally loading 기능 구현

--- src/pages/TodoDetail.jsx
+++ src/pages/TodoDetail.jsx
@@ -3,14 +3,17 @@ import { useParams } from 'react-router-dom'
 
 function TodoDetail() {
     const { id } = useParams()
-
     const [todo, setTodos] = useState({})
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         fetch(`https://dummyjson.com/todos/${id}`)
             .then((res) => res.json())
             .then((res) => setTodos(res))
+            .finally(() => setLoading(false))
     }, [])
+
+    if (loading) return <> 로딩중...</>
 
     return (
         <ul>
